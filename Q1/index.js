2b5be22a71f48dbb275c0bd6b593082c0d1577a8
Damilola Animashaun <omoprodigi@gmail.com>
2017-09-28 03:42:05 +0100

    Question 1

---
+++ Q1/index.js
@@ -0,0 +1,13 @@
+const validColors = 'abcdefghijklm'
+
+function printerError(input) {
+    let invalid = 0;
+    for(let char of [...input]){
+        if (!validColors.includes(char))
+            invalid++;
+    }
+    return `${invalid}/${[...input].length}`
+}
+
+var res = printerError('aaaxbbbbyyhwawiwjjjwwm');
+console.log(res);
